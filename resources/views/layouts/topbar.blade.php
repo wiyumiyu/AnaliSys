0dd684a9a93a41532695431ff43153f9ab22103e
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-01-24 14:12:44 -0600

    de de claro oscuro arreglado

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -40,12 +40,13 @@
 
                 {{-- MODO CLARO / OSCURO --}}
                 <div class="dark-mode-btn" id="toggleMode">
-                    <button class="btn header-btn active" id="lightModeBtn">
+
+                    <button class="btn header-btn active" id="darkModeBtn">
                         <i class="bi bi-brightness-high"></i>
                     </button>
-                    <button class="btn header-btn" id="darkModeBtn">
+                    <button class="btn header-btn " id="lightModeBtn">
                         <i class="bi bi-moon-stars"></i>
-                    </button>
+                    </button>                    
                 </div>
 
                 {{-- PERFIL --}}

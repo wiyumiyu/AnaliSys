3a48a7ccd7f7e3b22150c067e39d3f7e4f4374b9
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-01-26 12:11:59 -0600

    direccion relativa de usuario eliminada

--- resources/views/partials/sidebarx.blade.php
+++ resources/views/partials/sidebarx.blade.php
@@ -127,7 +127,7 @@
                         <a href="javascript:void(0)">Administración</a>
                     </li>
                     <li class="pe-slide-item">
-                        <a href="usuarios" class="pe-nav-link">
+                        <a href="{{ route('usuarios.index') }}" class="pe-nav-link">
                             Usuario
                         </a>
                     </li>

153bebe0d2fb96cfc8a340a63817f51ef302c3dd
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-02-28 20:20:37 -0600

    cambios en solicitud impresa

--- database/Tablas CIASI/tbm_solicitud_impresa.sql
+++ database/Tablas CIASI/tbm_solicitud_impresa.sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 3.5.7
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-02-2026 a las 20:57:01
--- Versión del servidor: 5.5.27
--- Versión de PHP: 5.4.7
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de datos: `bd_cia`
---
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbm_solicitud_impresa`
 --
-
+USE analisysbd;
 CREATE TABLE IF NOT EXISTS `tbm_solicitud_impresa` (
   `tbm_solicitud_impresa` int(11) NOT NULL AUTO_INCREMENT,
   `id_solicitud` varchar(10) COLLATE latin1_general_ci NOT NULL,
@@ -35,7 +14,75 @@ CREATE TABLE IF NOT EXISTS `tbm_solicitud_impresa` (
   `enviada_por` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`tbm_solicitud_impresa`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=8687 ;
+DROP PROCEDURE IF EXISTS sp_listar_reportes_clientes;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- drop procedure sp_listar_reportes_clientes
+DELIMITER $$
+
+CREATE PROCEDURE sp_listar_reportes_clientes (
+    IN p_periodo INT,
+    IN p_tipo INT,          -- 0 = pendientes, 1 = generadas
+    IN p_buscar VARCHAR(100)
+)
+BEGIN
+
+    SELECT 
+        s.id_solicitud,
+        s.numero,
+        s.fecha,
+        c.nombre,
+        CASE 
+            WHEN si.id_solicitud IS NULL THEN 'Pendiente'
+            ELSE 'Generada'
+        END AS estado_reporte
+
+    FROM tbm_solicitud s
+    left JOIN tbm_cliente c 
+        ON c.id_cliente = s.id_cliente
+
+    LEFT JOIN tbm_solicitud_impresa si
+        ON si.id_solicitud = s.id_solicitud
+
+    WHERE YEAR(s.fecha) = p_periodo
+
+    AND (
+        (p_tipo = 0 AND si.id_solicitud IS NULL)
+        OR
+        (p_tipo = 1 AND si.id_solicitud IS NOT NULL)
+    )
+
+    AND (
+        p_buscar IS NULL
+        OR p_buscar = ''
+        OR s.numero LIKE CONCAT('%', p_buscar, '%')
+        OR c.nombre LIKE CONCAT('%', p_buscar, '%')
+    )
+
+    ORDER BY s.fecha DESC;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_obtener_reporte_cliente (
+    IN p_id INT
+)
+BEGIN
+
+    SELECT 
+        s.*,
+        c.nombre
+    FROM tbm_solicitud s
+    INNER JOIN tbm_cliente c
+        ON c.id_cliente = s.id_cliente
+    WHERE s.id_solicitud = p_id;
+
+END$$
+
+DELIMITER ;
+
+CALL sp_listar_reportes_clientes(2026, 0, '');
+SELECT DISTINCT YEAR(2026) 
+FROM tbm_solicitud
+ORDER BY 1 DESC;
